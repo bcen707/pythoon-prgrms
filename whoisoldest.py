e66f51544b6200248d39c42e887497d338f04e43
unbillyvable <czarsalad707@gmail.com>
2022-05-05 16:28:56 -0700

    created whoisoldest.py

---
+++ whoisoldest.py
@@ -0,0 +1,9 @@
+def whoOldest(a1, a2, a3):
+    if a2<a3 and a1<a3:
+        print("The oldest person in the group is", a3)
+    elif a1<a2 and a3<a2:
+        print("The oldest person in the group is", a2)
+    elif a2<a1 and a3<a1:
+        print("The oldest person in the group is", a1)
+    else:
+        print("2 or more people in the group are of the same age.")
