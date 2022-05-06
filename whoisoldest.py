594b9165a635468fe56467b2642584c5a074010a
bcen707 <czarsalad707@gmail.com>
2022-05-05 22:50:36 -0700

    brainstorm cases for 2-way ties

--- whoisoldest.py
+++ whoisoldest.py
@@ -7,3 +7,10 @@ def whoOldest(a1, a2, a3):
         print("The oldest person in the group is", a1)
     else:
         print("2 or more people in the group are of the same age.")
+
+# how should I handle 2-way or 3-way ties???
+
+# 2way tie; 3 case checks:
+  a1 == a2 and a1 != a3 
+  a2 == a3 and a2 != a1
+  a1 == a3 and a1 != a2
